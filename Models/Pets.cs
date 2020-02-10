f175c3e2b79a6e38ed504a9a066a0b2cc01ca2c6
Ken Mckinley <km956216@ohio.edu>
2020-02-10 13:03:55 -0500

    hhh

---
+++ Models/Pets.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace km956216_MIS4200_.Models
+{
+    public class Pets
+    {
+        public int petsID { get; set; }
+        public string petName { get; set; }
+        public string description { get; set; }
+        public int vetId { get; set; }
+        public virtual Vets vets { get; set; }
+
+    }
+}
---
+++ Models/Vets.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace km956216_MIS4200_.Models
+{
+    public class Vets
+    {
+        public int vetID { get; set; }
+        public string vetFirstName { get; set; }
+        public string vetLastName { get; set; }
+
+        public string patients { get; set; }
+
+    }
+}
